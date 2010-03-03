123b6b919485b660eae2c59a29c8828a57b17362
Mark Sands <mark@Mark-Sandss-MacBook-Pro.local>
2010-03-03 01:17:27 -0600

    found API docs.

--- lib/shurl.rb
+++ lib/shurl.rb
@@ -1,7 +1,7 @@
-http://tinyarro.ws/info/api # probably the shortest
-http://is.gd/api_info.php
-http://krz.ch/?module=Pages&id=27
-http://idek.net/info/url-shortening-api
-http://ow.ly/url/shorten-url
-http://bit.ly/pages/tools/developer-tools/
+http://tinyarro.ws/info/api # http://tinyarro.ws/api-create.php?url=http://www.example.com
+http://is.gd/api_info.php # http://is.gd/api.php?longurl=http://www.example.com
+http://krz.ch/?module=Pages&id=27 # http://krz.ch/?module=ShortURL&file=Add&url=http%3A%2F%2Fvergleiche.ch%2Fmp3player%2Fueber_50gb_speicher%2Fapple_ipod_classic_120gb_silber_mb562
+http://idek.net/info/url-shortening-api # http://idek.net/shorten/?idek-api=true&idek-ref=your_app&idek-anchor=anchortag&idek-url=http://yourlonglink.com
+http://ow.ly/url/shorten-url # probably not... it's not out yet
+http://code.google.com/p/bitly-api/wiki/ApiDocumentation # have to have api key.. :\
 http://chilp.it/ # Shortening: http://chilp.it/api.php?url=http://www.example.com
